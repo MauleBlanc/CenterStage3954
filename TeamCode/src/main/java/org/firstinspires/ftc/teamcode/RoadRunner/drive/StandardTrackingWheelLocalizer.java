4a784dcf344234738536f4de4e63e6f559a30462
Azurepttf <owenyan06@gmail.com>
2024-01-09 17:14:36 +0100

    next is Lateral tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
@@ -34,7 +34,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.9449  ; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (enco  der) speed
 
-    public static double LATERAL_DISTANCE = 10; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 10.048; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 4; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1.031755;
